6043e48e52c3e150125a6c4ef83450d506dcdbb4
ezequielgmart <ezequielgm677@gmail.com>
2025-07-29 20:56:00 -0400

    fea: initial configuration

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+items = []
+
+@app.get("/")
+def root():
+    return items  
+
+@app.get("/items")
+def create_item(item:str):
+    items.append(item)
+    return items
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int) -> str:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
+
